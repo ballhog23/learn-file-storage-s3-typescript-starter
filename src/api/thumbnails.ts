4dd951f939f732633896f65116853b6d74f2a754
caleb <calebpirkle2@gmail.com>
2025-12-12 19:37:53 -0600

    removed get thumbnail endpoint? allowing certain mimetype uploads storing images on disk rather than base64 or in memory

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,42 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getInMemoryURL } from "./assets";
 import { getVideo, updateVideo } from "../db/videos";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string; };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string; };
@@ -69,9 +37,14 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   }
 
   const mediaType = file.type;
-
   if (!mediaType) {
     throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const allowedMimeType = ['image/jpeg', 'image/png'];
+
+  if (!allowedMimeType.includes(mediaType)) {
+    throw new BadRequestError("png or jpg files only");
   }
 
   const fileData = await file.arrayBuffer();
@@ -79,12 +52,13 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new Error("Error reading file data");
   }
 
-  videoThumbnails.set(video.id, {
-    data: fileData,
-    mediaType
-  });
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
 
-  const urlPath = getInMemoryURL(cfg, videoId);
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
+
+  const urlPath = getAssetURL(cfg, filename);
   video.thumbnailURL = urlPath;
   updateVideo(cfg.db, video);
 
