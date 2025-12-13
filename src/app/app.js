4d20f977ce333da9a74ec46b505bd94b480c77c6
caleb <calebpirkle2@gmail.com>
2025-12-12 19:53:54 -0600

    cache busting on front end

--- src/app/app.js
+++ src/app/app.js
@@ -266,7 +266,8 @@ function viewVideo(video) {
     thumbnailImg.style.display = "none";
   } else {
     thumbnailImg.style.display = "block";
-    thumbnailImg.src = video.thumbnailURL;
+    // cache busting by appending arbitrary query string to URL
+    thumbnailImg.src = `${video.thumbnailURL}?v=${Date.now()}`;
   }
 
   const videoPlayer = document.getElementById("video-player");
